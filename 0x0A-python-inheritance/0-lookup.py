87e1f894e0de2478771a2fbb4e254d71c0969662
MUROBII <usmanmurabi@gmail.com>
2024-06-29 14:50:31 +0100

    Implement a function that returns the list of
    available attributes and methods of an object:
    
    > Prototype: def lookup(obj):
    > Returns a list object
    > You are not allowed to import any module

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+"""Module Documentation
+
+This module contains a function that returns a list of all the
+attribute and methods of a given object
+
+"""
+
+
+def lookup(obj):
+    '''Gets the list of available attributes and methods of an object
+
+    Args:
+        obj: A Reference/Alias to the object
+
+    Return: A list containing the attributes of the object
+
+    '''
+    return (list(obj.__dict__))
