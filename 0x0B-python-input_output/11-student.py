17fdd862bfe0f9dc6e7959d7b86c2cf4c155e843
MUROBII <usmanmurabi@gmail.com>
2024-07-06 15:05:34 +0100

    Write a class Student that defines a student by:
    (based on 9-student.py)
    > Public instance attributes:
            * first_name
            * last_name
            * age
    > Instantiation with first_name, last_name and age: def __init__(self,
    first_name, last_name, age):
    
    > Public method def to_json(self, attrs=None): that retrieves a
    dictionary representation of a Student instance (same as
    8-class_to_json.py):
            * if attrs is a list of strings, only attribute names contained
              in this list must be retrieved.
            * Otherwise, all attributes must be retrieved
    > Public method def reload_from_json(self, json): that replaces all
    attributes of the Student instance:
    	* You can assume json will always be a dictionary
    	* A dictionary key will be the public attribute name
    	* A dictionary value will be the value of the public attribute
    > You are not allowed to import any module

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+"""A simple module containing A simple student class definition
+"""
+
+
+class Student:
+    """A simple Student class that defines student with 3
+    attributes.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing, 3 of the student attributes
+        """
+        self.first_name = first_name  #: str: Name
+        self.last_name = last_name  #: str: Surname
+        self.age = age  #: int: Student age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance
+
+        Returns: The dictionary representation of the Student
+        """
+        # Check, if the attrs, list is not none and contains string
+        # make a new dict
+        new_dict = {}
+        if attrs is not None and type(attrs) is list:
+            # popping attributes that are not in the list
+            for key in self.__dict__:
+                if key in attrs:
+                    new_dict[key] = self.__dict__[key]
+        # If new_dict is empty
+            return new_dict
+        # Else return the original dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+
+        Args:
+            json: The dictionary that contains the new attribute
+            and values of the object.
+        """
+        # If json is not an empty dict
+        if (len(json)):
+            self.__dict__ = json
