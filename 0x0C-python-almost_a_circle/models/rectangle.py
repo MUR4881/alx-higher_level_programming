d748ac848a178c1e20dde7a40fb3915041642700
MUROBII <usmanmurabi@gmail.com>
2024-07-12 12:37:13 +0100

    Update the class Rectangle by adding validation of
    all setter methods and instantiation (id excluded):
    
    If the input is not an integer, raise the TypeError exception with the
    message: <name of the attribute> must be an integer.
    	Example: width must be an integer
    If width or height is under or equals 0, raise the ValueError exception
    with the message: <name of the attribute> must be > 0. Example: width
    must be > 0
    If x or y is under 0, raise the ValueError exception with the message:
    <name of the attribute> must be >= 0. Example: x must be >= 0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,34 +24,45 @@ class Rectangle(Base):
             id: id passed to the super class instantiation
         """
         super().__init__(id)
-        self.__x = x  #: int: The x co-ordinate of the object
-        self.__y = y  #: int: The y co-ordinate of the object
-        self.__width = width  #: int the width of the rect object
-        self.__height = height  #: int the height of the rect obj
+        self.x = x  #: int: The x co-ordinate of the object
+        self.y = y  #: int: The y co-ordinate of the object
+        self.width = width  #: int the width of the rect object
+        self.height = height  #: int the height of the rect obj
 
     @property
     def x(self):
         """getter for the x co-ordinate
+
+            The setter ensures the value is
+            an integer and not lesser than 0
         """
         return self.__x
 
     @x.setter
     def x(self, x):
+        self.validate(x, 'x')
         self.__x = x
 
     @property
     def y(self):
         """getter for the y co-ordinate
+
+            The setter ensures the value is
+            an integer and not lesser than 0
         """
         return self.__y
 
     @y.setter
     def y(self, y):
+        self.validate(y, 'y')
         self.__y = y
 
     @property
     def width(self):
         """getter for the width dimension
+            The setter ensures the value is
+            an integer not lesser or equal
+            to zero
         """
         return self.__width
 
@@ -59,14 +70,37 @@ class Rectangle(Base):
     def width(self, width):
         """Setter for the width dimension
         """
+        self.validate(width, 'width')
         self.__width = width
 
     @property
     def height(self):
         """getter for the height attribute
+
+            The setter ensures the value is
+            an integer not lesser or equal
+            to zero
         """
         return self.__height
 
     @height.setter
     def height(self, height):
+        self.validate(height, 'height')
         self.__height = height
+
+    def validate(self, value, name):
+        """Helps validate width, height, x, y
+        attributes when setting
+
+        Args:
+            value: The value be assigned to the
+            attribute
+            name: The variable name of the attribute
+        """
+        if type(value) is int:
+            if name in ('x', 'y') and value < 0:
+                raise ValueError(f'{name} must be >= 0')
+            if name in ('width', 'height') and value <= 0:
+                raise ValueError(f'{name} must be > 0')
+        else:
+            raise TypeError(f"{name} must be an integer")
