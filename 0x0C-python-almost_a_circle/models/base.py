7a0505b9dbfc66046d3483f9a70c14ce76e06b7a
MUROBII <usmanmurabi@gmail.com>
2024-07-14 13:18:35 +0100

    Add Json->String Static method to base class:
    Update the class Base by adding the static method:
    	def to_json_string(list_dictionaries):
    that returns the JSON string representation of
    list_dictionaries:
    	* list_dictionaries is a list of dictionaries
    	* If list_dictionaries is None or empty, return the string:
    	  "[]"
    	* Otherwise, return the JSON string representation of
    	list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 
 """A simple module, entailing a simple Base class called Base
 """
+from json import load, loads, dump, dumps
 
 
 class Base:
@@ -25,3 +26,14 @@ class Base:
             self.id = self.__class__.__nb_objects  #: int: count or id
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Generates a JSON string representation
+        of list_dictionaries
+
+        Return: The json string
+        '''
+        if list_dictionaries is None:
+            return "[]"
+        return dumps(list_dictionaries)
