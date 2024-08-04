f29a984d290bef52531c26a019928b75dd178ab0
MUROBII <usmanmurabi@gmail.com>
2024-08-04 13:40:23 +0100

    List all records with a score >= 10:
    Write a script that lists all records with a score >= 10 in the table
    second_table of the database htbn_0c_0 in your MySQL server.
    
    	* Results should display both the score and the name (DESC)
    	* Records should be ordered by score (top first)
    	* The database name will be passed as an argument of the mysql
    	  command

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- filter and list records
+-- List all records with score >= 10
+SELECT score, name FROM second_table
+	WHERE score >= 10
+	ORDER BY score DESC;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Count a the number of value occurence
 -- Counts the number of records with id = 89 in the first_table
-SELECT count(id) FROM first_table WHERE id=89;
+SELECT COUNT(id) FROM first_table WHERE id=89;
