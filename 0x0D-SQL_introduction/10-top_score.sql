cfd767f1539d00a016abbd05506c07104b6952c9
MUROBII <usmanmurabi@gmail.com>
2024-08-05 12:22:35 +0100

    Made Some syntaxical changes and added test databases

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- lists all records of a table
 -- Lists all score name of the table second_table by the order of their score
 SELECT score, name FROM second_table
-	ORDER BY score DESC
+	ORDER BY score DESC;
