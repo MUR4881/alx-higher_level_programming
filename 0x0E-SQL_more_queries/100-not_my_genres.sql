54a84ce091a1a5ee63dde87412af07dd292e9f8d
MUROBII <usmanmurabi@gmail.com>
2024-08-08 08:33:39 +0100

    Use Except/Minus Instead of Not IN

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,15 +4,14 @@
 -- SOURCE hbtn_0d_tvshow.sql  # Import not needed, Imported when testing
 
 -- list all genres not relating to Dexter
-SELECT name
-  FROM tv_genres
-  WHERE name NOT IN  -- ALL Genres MINUS Genres Not under dexter
-	         (SELECT name
-  		  FROM tv_show_genres
-    		  JOIN tv_genres
-                    ON tv_genres.id = genre_id
-                  JOIN tv_shows
-                    ON tv_shows.id = show_id
-                 WHERE title = "Dexter"
-		)
+  SELECT name
+    FROM tv_genres
+  EXCEPT   --  SUBTRACT DOES RELATED TO DEXTER        
+  SELECT name
+    FROM tv_show_genres
+    JOIN tv_genres
+      ON tv_genres.id = genre_id
+    JOIN tv_shows
+      ON tv_shows.id = show_id
+   WHERE title = "Dexter"
 ORDER BY name;
