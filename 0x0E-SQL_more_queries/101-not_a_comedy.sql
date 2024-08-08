37b3c602473d063a0e7a36c230f9a39374ab830e
MUROBII <usmanmurabi@gmail.com>
2024-08-08 08:45:09 +0100

    Update 101-not_a_comdey.sql to 101-not_a_comedy:
    Also Except is Subtituted for NOT IN

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,16 +4,14 @@
 -- SOURCE hbtn_0d_tvshow.sql  # Import not needed, Imported when testing
 
 -- list all NON Comedy show in the database `hbtn_0d_tvshows`
-SELECT title
-  FROM tv_shows
- WHERE title
-NOT IN
-  (SELECT title
+  SELECT title
+    FROM tv_shows
+  EXCEPT  -- Filtering OF those titles related to Genre comedy
+  SELECT title
     FROM tv_show_genres
     JOIN tv_genres
       ON tv_genres.id = genre_id
     JOIN tv_shows
       ON tv_shows.id = show_id
    WHERE name = "Comedy"
-  )
 ORDER BY title ASC;
