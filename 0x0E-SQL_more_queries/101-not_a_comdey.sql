49e5fa2149f44974ca8e3afdaf74b6f95e832527
MUROBII <usmanmurabi@gmail.com>
2024-08-08 03:54:40 +0100

    List ALL NON Comedy Shows, having No bit o comedy:
    Import the database dump from hbtn_0d_tvshows to your MySQL Server:
    
    Write a script that lists all shows without the genre Comedy in the
    database hbtn_0d_tvshows.
    
      * The tv_genres table contains only one record where name = Comedy
        (but the id can be different)
    
      * Each record should display: tv_shows.title
      * Results must be sorted in asceding order by the show title
      * You can use a maximum of two SELECT statement
      * The database name will be passed as an argument of the mysql command

---
+++ 0x0E-SQL_more_queries/101-not_a_comdey.sql
@@ -0,0 +1,19 @@
+-- Import database dump and fetch column (of a table) by(of) another
+
+-- Import the database dump
+-- SOURCE hbtn_0d_tvshow.sql  # Import not needed, Imported when testing
+
+-- list all NON Comedy show in the database `hbtn_0d_tvshows`
+SELECT title
+  FROM tv_shows
+ WHERE title
+NOT IN
+  (SELECT title
+    FROM tv_show_genres
+    JOIN tv_genres
+      ON tv_genres.id = genre_id
+    JOIN tv_shows
+      ON tv_shows.id = show_id
+   WHERE name = "Comedy"
+  )
+ORDER BY title ASC;
