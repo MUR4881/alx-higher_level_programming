7daee36a49bb138531fcde026d142df846ba2609
MUROBII <usmanmurabi@gmail.com>
2024-08-08 02:25:46 +0100

    List all comedy movies:
    Import the database dump from hbtn_0d_tvshows to your MySQL server:
    
    Write a script that lists all Comedy shows in the database
    hbtn_0d_tvshows:
    
    	* The tv_genres table contains only one record where name =
    	  Comedy (but id can be different)
    
    	* Each record should display: tv_shows.title
    	* Results must be sorteed in ascending order by the show title
    	* You can use only one SELECT statement
    	* The database name will be passed as an argument of the mysql
    	  command

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,14 @@
+-- Import database dump and fetch column (of a table) by(of) another
+
+-- Import the database dump
+-- SOURCE hbtn_0d_tvshow.sql  # Import not needed, Imported when testing
+
+-- list all Comedy show in the database `hbtn_0d_tvshows`
+  SELECT title
+    FROM tv_show_genres
+    JOIN tv_genres
+      ON tv_genres.id = genre_id
+    JOIN tv_shows
+      ON tv_shows.id = show_id
+   WHERE name = "Comedy"
+ORDER BY title ASC;
