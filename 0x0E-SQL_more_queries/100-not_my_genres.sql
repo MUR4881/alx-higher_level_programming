c05320997ffd481ffe7c5e0a95b6c74e85bee6c0
MUROBII <usmanmurabi@gmail.com>
2024-08-08 03:45:11 +0100

    List all genres not linked to the show Dexter:
    Import the database dump from hbtn_0d_tvshows to your MySQL server:
    
    Write a script that uses the hbtn_0d_tvshows database to list all genres
    not linked to the show Dexter
    
      * The tv_shows table contains only one record where title = Dexter
        (but the id can be different)
      * Each record should display: tv_genres.name
      * Results must be sorted in ascending order by the genre name
      * You can use a maximum of two SELECT statement
      * The database name will be passed as an argument of the mysql command

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,18 @@
+-- Import database dump and fetch column (of a table) by(of) another
+
+-- Import the database dump
+-- SOURCE hbtn_0d_tvshow.sql  # Import not needed, Imported when testing
+
+-- list all genres not relating to Dexter
+SELECT name
+  FROM tv_genres
+  WHERE name NOT IN  -- ALL Genres MINUS Genres Not under dexter
+	         (SELECT name
+  		  FROM tv_show_genres
+    		  JOIN tv_genres
+                    ON tv_genres.id = genre_id
+                  JOIN tv_shows
+                    ON tv_shows.id = show_id
+                 WHERE title = "Dexter"
+		)
+ORDER BY name;
